1326854e132a15bebc04dcfb42e6f2e71a2a111a
45akshat <akshatparmar78543@gmail.com>
2023-03-06 13:09:26 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const express = require('express');
+const app = express();
 const puppeteer = require('puppeteer');
 
-(async () => {
+app.get('/ss', async (req, res) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto('https://google.com');
+  const buffer = await page.screenshot();
   
   await browser.close();
-})();
+  
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': buffer.length
+  });
+  res.end(buffer);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
